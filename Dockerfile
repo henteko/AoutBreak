66d1135ed9a705d06d34b0affea2d287be2cfe1e
vagrant <vagrant@precise64.(none)>
2014-01-16 18:56:54 +0000

    init

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM ubuntu:12.10
+MAINTAINER henteko
+
+RUN apt-get update
+RUN apt-get install -y curl git build-essential ruby1.9.3 libsqlite3-dev
+RUN apt-get install -y nodejs libv8-dev
+RUN gem install rubygems-update --no-ri --no-rdoc
+RUN update_rubygems
+RUN gem install bundler sinatra --no-ri --no-rdoc
+
+ADD rails_sample /opt/railsapp
+
+WORKDIR /opt/railsapp
+RUN bundle install
---
+++ test.rb
@@ -0,0 +1,23 @@
+require 'redis'
+
+BASE_IP = "127.0.0.1"
+BASE_HOST = "testdocker.com"
+base_port = 3000
+
+redis = Redis.new(:host => "127.0.0.1", :port => 6379)
+
+port = base_port + redis.dbsize.to_i
+puts "Input app name: "
+app_name = STDIN.gets
+app_name = app_name.chomp
+
+exit if app_name == ""
+
+
+system("sudo docker build -t henteko/rails .")
+run_command = "sudo docker run -d -p " + port.to_s + ":3000 henteko/rails bundle exec rails s"
+system(run_command)
+
+puts app_name + '.' + BASE_HOST
+puts BASE_IP + ':' + port.to_s
+redis.set(app_name + '.' + BASE_HOST, BASE_IP + ':' + port.to_s)
